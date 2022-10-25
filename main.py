4ba0eed23e331ed11b6573b3d23bafcef0dc7ebe
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2022-10-25 20:42:54 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# Python program to print Calendar
+
+# Python built-in calender function
+import calendar
+
+# Enter in format - 2022, 2010, 1998 etc.
+year = int(input("Enter Year: "))
+
+# Enter in format - 03, 12, 01 etc.
+month = int(input("Enter Month: "))
+
+# Calendar to be printed
+print(calendar.month(year, month))
+
+#Coded with ❤️ by Vector
